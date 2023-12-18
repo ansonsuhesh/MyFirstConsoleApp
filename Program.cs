7bd7f496fa9f15f048e22c3407360a491073f29f
ANSON SUHESH <ansonsuhesh@outlook.com>
2023-12-18 23:11:06 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            OperatorExamples();
+        }
+
+        private static void OperatorExamples()
+        {
+            int width = 3;
+            width++;
+            int height = 2 + 4;
+            int area = width * height;
+            Console.WriteLine(area);
+            Console.ReadLine();
+
+            string result = "The area";
+            result = result + " is " + area;
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+            bool truthValue = true;
+            Console.WriteLine(truthValue);
+            Console.ReadLine();
+
+        }
+    }
+}
